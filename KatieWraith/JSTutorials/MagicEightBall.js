a23fe96923c37bd2e47edcdfc79ae0529da0d72c
kwraith <kwraith97@gmail.com>
2018-10-05 13:52:34 -0700

    Create MagicEightBall.js

---
+++ KatieWraith/JSTutorials/MagicEightBall.js
@@ -0,0 +1,36 @@
+let userName = '';
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+let userQuestion = 'Should I do homework?';
+console.log(userQuestion);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch(randomNumber){
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+  default:
+    eightBall = 'Try again';
+    break;
+}
+console.log(eightBall);
