5cc75fd19b25c747fb5e0a02a7f62861b9f83e1e
Kevin Tran <kevintrancs@gmail.com>
2019-03-26 19:10:41 -0700

    I Fix pipeline for ml1

--- data/news.py
+++ data/news.py
@@ -15,19 +15,19 @@ logger = logging.getLogger(__name__)
 with open('../server/constants.json') as f:
     CONSTANTS = json.load(f)
 
-headers = {"Ocp-Apim-Subscription-Key": CONSTANTS['azure_endpoint']}
+headers = {"Ocp-Apim-Subscription-Key": CONSTANTS['azure_key']}
 db = boto3.resource('dynamodb')
 table = db.Table('NewsHashed')
 
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
-#nltk.download('wordnet')
+# nltk.download('wordnet')
 import re
-from nltk.corpus import stopwords 
-#from nltk.tokenize import word_tokenize, sent_tokenize 
+from nltk.corpus import stopwords
+#from nltk.tokenize import word_tokenize, sent_tokenize
 #from nltk.stem import WordNetLemmatizer
 import math
 
@@ -41,22 +41,25 @@ stopwords = nltk.corpus.stopwords.words('english')
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
+
 def pre_process(text):
-    
+
     # lowercase
-    text=text.lower()
-    
-    #remove tags
-    text=re.sub("&lt;/?.*?&gt;"," &lt;&gt; ",text)
-    
+    text = text.lower()
+
+    # remove tags
+    text = re.sub("&lt;/?.*?&gt;", " &lt;&gt; ", text)
+
     # remove special characters and digits
-    text=re.sub("(\\d|\\W)+"," ",text)
-    
+    text = re.sub("(\\d|\\W)+", " ", text)
+
     return text
+
 
 def sort_coo(coo_matrix):
     tuples = zip(coo_matrix.col, coo_matrix.data)
-    return sorted(tuples, key=lambda x:(x[1], x[0]), reverse=True)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
 
 def extract_topn_from_vector(feature_names, sorted_items, topn=10):
     sorted_items = sorted_items[:topn]
@@ -110,24 +113,26 @@ def get_keyword(term):
         d['thumbnail'] = article['image']['thumbnail']['contentUrl'] if 'image' in article else " "
         d['query_use'] = queried
 
-        df = pd.Dataframe(data=d)
-        df['text'] = df['name'] + " " + df['description']
-        df['text'] = df['text'].apply(lambda x:pre_process(x))
-        docs = df['text'].tolist()
+        processable = d['name'] + " " + d['description']
+
+        text = processable.split()
+
+        for i in text:
+            i = pre_process(i)
+        docs = text
         cv = CountVectorizer(max_df=0.85, stop_words=stopwords)
         word_count_vector = cv.fit_transform(docs)
-        tfidf_transformer = TfidfTransformer(smooth_idf=True,use_idf=True)
+        tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
         tfidf_transformer.fit(word_count_vector)
 
         feature_names = cv.get_feature_names()
-        doc = docs[0]
-        tf_idf_vector = tfidf_transformer.transform(cv.transform([doc]))
+        tf_idf_vector = tfidf_transformer.transform(cv.transform(docs))
         sorted_items = sort_coo(tf_idf_vector.tocoo())
-        doc_keywords = extract_topn_from_vector(feature_names,sorted_items,10)
+        doc_keywords = extract_topn_from_vector(
+            feature_names, sorted_items, 10)
         kw_list = []
         for k in doc_keywords:
             kw_list.append(k)
-
         d['keywords'] = kw_list
         data.append(d)
     return data
